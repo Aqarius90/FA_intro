1f54cb0a4d6086122c8ed1db113b2c67e24546bc
Unknown <aqarius90@gmail.com>
2017-01-12 16:31:34 +0100

    rc1.1

--- js/js.js
+++ js/js.js
@@ -5,6 +5,11 @@ $(document).ready(function() {
         var target = "#" + $(this).data("target");
         $(".tohide").not(target).hide();
         $(target).show();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
     });
+});
 
+$(document).ready(function() {
+    $("#Intro").show();
+    $("html, body").animate({ scrollTop: 0 }, "slow");
 });
